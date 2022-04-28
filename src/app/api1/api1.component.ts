9c70a69ecf55ddfd1215681fc6279643527c6c9f
Rodrigo Urtecho <A16003872@alumnos.uady.mx>
2022-04-27 21:25:55 -0500

    fixed scss to css

--- src/app/api1/api1.component.ts
+++ src/app/api1/api1.component.ts
@@ -4,7 +4,7 @@ import { ValidaremailService } from '../services/validaremail.service';
 @Component({
   selector: 'app-api1',
   templateUrl: './api1.component.html',
-  styleUrls: ['./api1.component.scss']
+  styleUrls: ['./api1.component.css']
 })
 export class Api1Component implements OnInit {
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'api_project';
